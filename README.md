bfc7a60cb151d199a4a5bbf879aa45f63c993d96
abkolan <abkolan@gmail.com>
2019-04-25 19:30:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,169 @@
+# stream-alerts-improved
+
+### Problem
+Set off an alert, when a word occurs for more than X number of times in a sliding window of M minutes in a stream of text (like Tweets)
+
+### Assumptions
+* Time at which the tweet is generated and the time at which the tweet is processed is the same. The difference between them is zero. 
+* This solution is running on a system that has has infinite resources (Not a distributed Systems problem)
+* No usage of any third party softwares whether open source or properitary. (For example: Map-Reduce, Spark, Storm or Kafka QL.
+* No usage of any third party libraries.
+
+
+### Approach
+
+![](https://github.com/abkolan/stream-alerts/blob/master/media/image.png?raw=true)
+
+#### Part 1 (of 2) - Storing the values in an array 
+Let the number of seconds in the interval `M` be `n`. We will maintain an array, `A` of size `n+1` against every word in a KV map. This array would be used to store the occurences of the word. To maintain the sliding window, we would use the array in a circular fashion. This would ensure that only values that are valid for a particular window would remain in the array. To ensure that the values of the current sliding window is maintained, we also store the lastupdated timestamp in the array. Using this we would be able to infer the current state of the values in array. Explained in detail below. 
+
+#### Part 2 (of 2) - Storing the array as a Segment Tree for updates in O(log n) time.
+
+We would be doing 3 operations on the array. 
+
+* Increment a bucket/index 
+* Reset a range to zero
+* Get sum of the range of values. (0 to n complete range in our case)
+
+We use a Segment Tree for the three operations, as all the three operations above can be done in O(log n) time. Get sum in our case is in the complete range it would be in O(1) time, as we have done all the heavy lifting during the updates.
+
+#### Simple simulation of single sliding window using both concepts
+A simple simulation (single instance of a sliding window) of both the concepts can be found [here](https://gist.github.com/abkolan/d786e261752d2ae76faa11fcb1645aa4).
+
+
+#### Part 1 - Details
+This is easier to explain this with an example. Consider an interval of 5 seconds, i.e `n=5`. To maintain a sliding window an array of size `n+1` is created. In our case an array of size `6`. 
+
+
+**Event Id** is the is the `id` of the event, does not have any significance, only used for the ease of explaining the concept. **Timestamp** is the timestamp of the event in seconds. **Value** is the value emitted at the timestamp, word frequency in our case. 
+
+In the simplest case, the events occur in succession. Consider 5 such events, that are seperated by 1 second each. 
+
+|Event Id| Timestamp    | Value | 
+|:------:|:------------:|:-----:| 
+|E1      | 00           | 1     |    
+|E2      | 01           | 1     |      
+|E3      | 02           | 1     |   
+|E4      | 03           | 1     |   
+|E5      | 04           | 1     |   
+
+`A` is populated by modding the **Timestamp** by the interval to determine the index or the bucket, where the value is set. (Ignore the last index, we will cover that later)  
+Event E1, generated at timestamp `00` would go to the bucket `0`, Event E2 generated at the timestamp `01` would go the bucket `1` and so on. And, for this simple case the array `A` would look like this. 
+
+Bucket|0  |1  |  2|  3|  4|  5|
+------|---|---|---|---|---|---|
+Value |1  |1  |1  |1  |1  |-  |
+Event |E1 |E2 |E3 |E4 |E5 |-  |
+
+The sum of all the values occurred in a window can be computed by `Sum(A[0:4])`. In the above case the sum is `5`. 
+
+Consider an extension of the above case, where an Event 6 occurs at `05`.
+
+|Event Id| Timestamp    | Value | 
+|:------:|:------------:|:-----:| 
+|E1      | 00           | 1     |    
+|E2      | 01           | 1     |      
+|E3      | 02           | 1     |   
+|E4      | 03           | 1     |   
+|E5      | 04           | 1     |
+|E6      | 05           | 1     |    
+
+Since we are only concerned about the events that happened in last 5 seconds, we can overwrite value at bucket `0`. And, sum would give us the value of the sum in the sliding window of 5 seconds, that is from event E2 to E6. The array `A` would like this. 
+
+Bucket|0  |1  |  2|  3|  4|  5|
+------|---|---|---|---|---|---|
+Value |1  |1  |1  |1  |1  |-  |
+Event |E6 |E2 |E3 |E4 |E5 |-  |
+
+To accurately maintain these values in all the cases, we would also need the timestamp of last seen event. This is stored in the last index of the array. 
+
+Consider these events.
+
+|Event Id| Timestamp    | Value | 
+|:------:|:------------:|:-----:| 
+|E1      | 00           | 1     |    
+|E2      | 01           | 1     |      
+|E3      | 07           | 1     |   
+
+At the end of processig of Event `E2`. The array `A` would look like. The last index `5` containing the value of the timestamp of `E2`. 
+
+Bucket|0  |1  |  2|  3|  4|  5|
+------|---|---|---|---|---|---|
+Value |1  |1  |0  |0  |0  |01 |
+Event |E1 |E2 |-- |-- |-- |-- |
+
+When Event `E3` is processed, we check for the lastupdated timestamp. If difference between the current timestamp and the lastupdated timestamp is greater than the interval. We reset, all the buckets to zero and then update the current bucket. At the end of processing of `E3`. The array `A` would look like. 
+
+Bucket|0  |1  |  2|  3|  4|  5|
+------|---|---|---|---|---|---|
+Value |0  |0  |0  |0  |0  |01 |
+Event |-- |-- |E3 |-- |-- |-- |
+
+This leaves us with one pending usecase, when the events are not sequential. 
+Consider these sample events.
+
+|Event Id| Timestamp    | Value | 
+|:------:|:------------:|:-----:| 
+|E1      | 00           | 1     |    
+|E2      | 01           | 1     |      
+|E3      | 04           | 1     |   
+
+At the end of processig of Event `E2`. The array `A` would look like. The last index `5` containing the value of the timestamp of `E2`. 
+
+Bucket|0  |1  |  2|  3|  4|  5|
+------|---|---|---|---|---|---|
+Value |1  |1  |0  |0  |0  |01 |
+Event |E1 |E2 |-- |-- |-- |-- |
+
+When the event `E3` is processed, since the last updated timestamp is 01, and the current timestamp is 04. We would have to ensure that the buckets bodering that rage i.e 3-4 are reset to indicate that there were no events generated during that time. Indicated by `R`. 
+
+Bucket|0  |1  |  2|  3|  4|  5|
+------|---|---|---|---|---|---|
+Value |1  |1  |0  |0  |1  |04 |
+Event |E1 |E2 |R  |R  |E4 |-- |
+
+A simulation of this can be found in a gist [here](https://gist.github.com/abkolan/d786e261752d2ae76faa11fcb1645aa4).
+
+### Building and Running
+**Prerequisites**
+ 
+* JAVA on UNIX/Linux or Mac or Linux Substem for Windows<sup>*</sup>.
+* bash + GNU Core Utilities for simulation.  
+
+
+<sup>*</sup>not tested on Linux Substem on Windows
+#### Running the project
+
+1. **stream-alerts** is a java maven project, the project can be built into an executable jar by using.  
+`mvn package`
+
+2. This repo comes with a sample tweets (From Kaggle, the dataset has been modified only to include tweet text as is).  
+The sample tweets can be extracted by running:   
+`tar -xf sample-tweets.tar.gz`
+
+3. This repo also comes with a script `gen-tweets.sh` that would randomly generate 1000000 tweets per second from the `sample-tweets` file. Tweets per minute can be modifed in the `gen-tweets.sh`
+
+4. Run the simulation
+`./gen-tweets.sh | 
+java -jar target/stream-alert-0.0.1-jar-with-dependencies.jar <NUM-OCCURRENCES> <TIME-INTERVAL-IN-MIN> <IGNORE-STOP-WORDS>`
+
+
+  Example:
+  `./gen-tweets.sh | java -jar target/stream-alert-0.0.1-jar-with-dependencies.jar 10000 1 true`
+  
+  Excerpt of the output
+  ```
+   ALERT
+   back
+   ALERT
+   love
+   ALERT
+   good
+   ALERT
+   time
+   ALERT
+  going
+```
+
+  The above example would print out alerts when a non-stopword word occurrences is equal to or greater than 10000 for a sliding period of 1 minute, from the simulated tweets The counts are accurate upto a second.
+
---
+++ src/main/java/net/semikolan/Main.java
@@ -0,0 +1,286 @@
+package net.semikolan;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+
+
+public class Main {
+
+  private static int ALERT_NUM_OCCURRENCES = 5;
+  private static int TIME_INTERVAL_MINUTES = 1;
+
+  private final static int INTERVAL = TIME_INTERVAL_MINUTES * 60;
+
+  private static final HashMap<String, long[]> wordMap = new HashMap<>();
+
+  private static boolean considerStopWords = false;
+
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    for (String arg : args) {
+      System.out.println(arg);
+    }
+    if (args.length == 1 && args[0].equals("test")) {
+   // if (true) {
+      driver();
+    } else {
+      if (args.length == 3) {
+        ALERT_NUM_OCCURRENCES = Integer.parseInt(args[0]);
+        TIME_INTERVAL_MINUTES = Integer.parseInt(args[1]);
+        considerStopWords = Boolean.parseBoolean(args[2]);
+      }
+      // Reading the stream from STDOUT, used to pipe random tweets
+      InputStreamReader isReader = new InputStreamReader(System.in);
+      BufferedReader bufReader = new BufferedReader(isReader);
+      String inputStr = "";
+      long epoch = 0;
+      while (inputStr != null) {
+        try {
+          inputStr = bufReader.readLine();
+          epoch = System.currentTimeMillis();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        if (inputStr != null) {
+          process(inputStr, epoch);
+        } else {
+          System.out.println("<empty>");
+        }
+      }
+    }
+  }
+
+  //A Test driver
+  private static void driver() throws ExecutionException, InterruptedException {
+    /*System.out.println("Test mode");
+    ALERT_NUM_OCCURRENCES = 5;
+    TIME_INTERVAL_MINUTES = 1;
+    System.out.println("Sending Mock Data");
+    process("When you sense there’s something more", System.currentTimeMillis());
+    Thread.sleep(1000);
+    process("when the life that used to satisfy you no longer seems enough", System.currentTimeMillis());
+    Thread.sleep(2000);
+    process("when security seems suffocating and pleasures lose their taste", System.currentTimeMillis());
+    Thread.sleep(3000);
+    process("when dreams of success can’t motivate you anymore", System.currentTimeMillis());
+    Thread.sleep(500);
+    process("When you ache with a sadness that doesn’t seem to have a source", System.currentTimeMillis());
+    process("when you feel a hunger you’ve never known before", System.currentTimeMillis());
+    Thread.sleep(10000);*/
+
+    //Test case for within in window
+    process("this",System.currentTimeMillis());
+    Thread.sleep(1000);
+    process("this",System.currentTimeMillis());
+    Thread.sleep(1000);
+    process("this",System.currentTimeMillis());
+    Thread.sleep(1000);
+    process("this",System.currentTimeMillis());
+    Thread.sleep(56 * 1000);
+    process("this",System.currentTimeMillis());
+    System.out.println("DONE");
+    Thread.sleep(5 * 1000);
+  }
+
+  private static void process(String inputStr, long epoch) throws ExecutionException {
+    inputStr = inputStr.toLowerCase();
+    StringTokenizer tokenizer = new StringTokenizer(inputStr, " \t\r\n.?!\",");
+    while (tokenizer.hasMoreElements()) {
+      String word = tokenizer.nextElement().toString();
+      if (considerStopWords) {
+        if (!word.isEmpty() && !stopWords.contains(word) && !word.startsWith("@")) {
+          eval(word, epoch);
+        }
+      } else {
+        if (!word.isEmpty() && !word.startsWith("@")) {
+          eval(word, epoch);
+        }
+      }
+    }
+  }
+
+  private static void eval(String word, long epoch) throws ExecutionException {
+    //Check wordMap contains this word if it doesn't initialize one
+    long[] tree;
+    if (wordMap.containsKey(word)) {
+      tree = wordMap.get(word);
+    } else {
+      tree = initTreeForSize(INTERVAL);
+      wordMap.put(word, tree);
+    }
+
+    //Extract seconds from epoch
+    long seconds = epoch / 1000; //Extract seconds
+    int bucket = (int) seconds % INTERVAL; //Get the bucket for this interval
+
+    //Check the last updated ts
+    long lastUpdatedTS = tree[tree.length - 1];
+
+    if (lastUpdatedTS == 0) {
+      // never updated, word seen for the first time
+      // SegTree set
+      setRange(tree, INTERVAL, bucket, bucket, 1);
+    } else if (lastUpdatedTS == seconds) {
+      // value has to be bumped up for the same
+      incrRange(tree, INTERVAL, bucket, bucket, 1);
+    } else if ((int) (seconds - lastUpdatedTS) >= INTERVAL) {
+      //reset all values older than the interval to 0
+      setRange(tree, INTERVAL, 0, INTERVAL - 1, 0); //TODO: Optimize this
+
+      //set the current interval bucket to 1
+      setRange(tree, INTERVAL, bucket, bucket, 1);
+    } else if ((int) (seconds - lastUpdatedTS) == 1) {
+      //Value is immediately after the previous timestamp, no updations needed
+      // SegTree set
+      setRange(tree, INTERVAL, bucket, bucket, 1);
+    } else { //currentTS is within the interval but there are gaps
+      int lastUpdatedBucket = (int) lastUpdatedTS % INTERVAL;
+
+      //Reset values from lastUpdatedBucket to current bucket
+      if (lastUpdatedBucket > bucket) {
+        //Circular update needed
+
+        // Part 1 lastBucket till the end
+        // SegTree set range
+        setRange(tree, INTERVAL, lastUpdatedBucket + 1, INTERVAL - 1, 0);
+
+        // Part 2 0 to current bucket
+        // SegTree set range
+        setRange(tree, INTERVAL, 0, bucket - 1, 0);
+      } else {
+        // SegTree set range
+        setRange(tree, INTERVAL, lastUpdatedBucket + 1, bucket - 1, 0);
+      }
+      // Set current value
+      // SegTree set
+      setRange(tree, INTERVAL, bucket, bucket, 1);
+    }
+
+    //update lastUpdatedTS
+    tree[tree.length - 1] = seconds;
+
+
+    //Compute the sum of the Window counter
+    int sum = (int) getSum(tree, INTERVAL, 0, INTERVAL - 1);
+    if (sum >= ALERT_NUM_OCCURRENCES) {
+      System.out.printf("ALERT generated for word = %s\n", word);
+    }
+  }
+
+
+  private static void incrRange(long tree[], int n, int us, int ue, int val) {
+    incrRangeUtil(tree, 0, 0, n - 1, us, ue, val);
+  }
+
+  private static void incrRangeUtil(long[] tree, int si, int ss, int se, int us,
+                                    int ue, int val) {
+    // out of range
+    if (ss > se || ss > ue || se < us)
+      return;
+
+    // Current node is a leaf node
+    if (ss == se) {
+      // Increment by val to current node
+      tree[si] += val;
+      return;
+    }
+
+    // If not a leaf node, increment for this node's children.
+    int mid = (ss + se) / 2;
+    incrRangeUtil(tree, si * 2 + 1, ss, mid, us, ue, val);
+    incrRangeUtil(tree, si * 2 + 2, mid + 1, se, us, ue, val);
+
+    // Use the result of children calls to compute this node's sum
+    tree[si] = tree[si * 2 + 1] + tree[si * 2 + 2];
+  }
+
+  private static void setRange(long[] tree, int n, int us, int ue, int value) {
+    setRangeUtil(tree, 0, 0, n - 1, us, ue, value);
+  }
+
+  private static void setRangeUtil(long[] tree, int si, int ss, int se, int us, int ue, int value) {
+    // out of range
+    if (ss > se || ss > ue || se < us)
+      return;
+
+    // Current node is a leaf node
+    if (ss == se) {
+      // Set the value to the current node
+      tree[si] = value;
+      return;
+    }
+
+    // If not a leaf node, set for this node's children.
+    int mid = (ss + se) / 2;
+    setRangeUtil(tree, si * 2 + 1, ss, mid, us, ue, value);
+    setRangeUtil(tree, si * 2 + 2, mid + 1, se, us, ue, value);
+
+    // Use the result of children calls to update this node's sum
+    tree[si] = tree[si * 2 + 1] + tree[si * 2 + 2];
+  }
+
+  private static long[] initTreeForSize(int n) {
+    //Compute Height of the Segment tree
+    int x = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+
+    //Compute the max height of the Segtree
+    int max = 2 * (int) Math.pow(2, x) - 1;
+
+    //Allocate a new array for max+1
+    //The last index is used to store the last updated timestamp
+    long[] tree = new long[max + 1];
+    return tree;
+  }
+
+  private static long getSum(long[] tree, int n, int qs, int qe) {
+    // Check for bounds
+    if (qs < 0 || qe > n - 1 || qs > qe) {
+      System.out.println("Out of Bounds");
+      //TODO: Raise exception
+      return -1;
+    }
+    return getSumUtil(tree, 0, n - 1, qs, qe, 0);
+  }
+
+  private static long getSumUtil(long[] tree, int ss, int se, int qs, int qe, int si) {
+    // If segment of this node is a part of given range, then return
+    // the sum of the segment
+    if (qs <= ss && qe >= se)
+      return tree[si];
+
+    // If segment of this node is outside the given range
+    if (se < qs || ss > qe)
+      return 0;
+
+    // If a part of this segment overlaps with the given range
+    int mid = getMid(ss, se);
+    return getSumUtil(tree, ss, mid, qs, qe, 2 * si + 1) +
+        getSumUtil(tree, mid + 1, se, qs, qe, 2 * si + 2);
+  }
+
+  private static int getMid(int s, int e) {
+    return s + (e - s) / 2;
+  }
+
+
+  //NLTK's list of english stopwords
+  private static String[] stopWordsEnglish = {
+      "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves",
+      "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their",
+      "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are",
+      "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an",
+      "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about",
+      "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up",
+      "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when",
+      "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor",
+      "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don't", "should",
+      "now", "i'm", "i", "me"
+  };
+  private static final Set<String> stopWords = new HashSet<>(Arrays.asList(stopWordsEnglish));
+
+
+}
